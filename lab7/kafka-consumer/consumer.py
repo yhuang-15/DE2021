62d31e45b64d2d91d7b9586ad66216f180df3176
IndikaKuma <ipkumarawd@yahoo.com>
2021-10-31 01:42:24 +0200

    updated kafka consumer

--- lab7/kafka-consumer/consumer.py
+++ lab7/kafka-consumer/consumer.py
@@ -7,8 +7,8 @@ def read_from_topic(kafka_consumer, topic):
         print(msg.value.decode("utf-8"))
 
 
-def read_from_topic_with_partition(kafka_consumer, topic1, topic2):
-    kafka_consumer.assign([TopicPartition(topic1, 1), TopicPartition(topic2, 1)])
+def read_from_topic_with_partition(kafka_consumer, topic):
+    kafka_consumer.assign([TopicPartition(topic, 1)])
     for msg in kafka_consumer:
         print(msg)
 
@@ -25,6 +25,6 @@ def read_from_topic_with_partition_offset(kafka_consumer, topic):
 if __name__ == '__main__':
     consumer = KafkaConsumer(bootstrap_servers='104.198.60.211:9092',
                              auto_offset_reset='earliest',
-                             consumer_timeout_ms=1000)
+                             consumer_timeout_ms=10000)
     print(consumer.topics())
-    read_from_topic_with_partition(consumer, 'word')
+    read_from_topic(consumer, 'word')
